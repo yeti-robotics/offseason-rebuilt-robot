9c421974ac6524b8cdf1457d5af57788f1c81f37
amitnchoudhary <amazingamitty@gmail.com>
2026-09-04 16:44:16 -0400

    syscore busses

--- src/main/java/frc/robot/subsystems/intake/IntakeIOTalonFX.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeIOTalonFX.java
@@ -16,8 +16,8 @@ public class IntakeIOTalonFX implements IntakeIO {
     private final VoltageOut voltageRequest = new VoltageOut(0);
 
     public IntakeIOTalonFX() {
-        rightIntakeMotor = new TalonFX(IntakeConfigs.RIGHT_INTAKE_MOTOR_ID, Constants.can_s1);
-        leftIntakeMotor = new TalonFX(IntakeConfigs.LEFT_INTAKE_MOTOR_ID, Constants.can_s1);
+        rightIntakeMotor = new TalonFX(IntakeConfigs.RIGHT_INTAKE_MOTOR_ID, Constants.CAN_S1);
+        leftIntakeMotor = new TalonFX(IntakeConfigs.LEFT_INTAKE_MOTOR_ID, Constants.CAN_S1);
         if (Robot.isSimulation()) {
             PhysicsSim.getInstance().addTalonFX(rightIntakeMotor);
             PhysicsSim.getInstance().addTalonFX(leftIntakeMotor);
